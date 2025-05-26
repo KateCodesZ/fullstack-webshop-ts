39c167aa67a1a43bf146e522975d7b5f55024113
Kate <katjas.emailg@gmail.com>
2025-05-26 22:28:42 +0200

    verified successful data fetching from backend to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+import api from './api';
+
 function App() {
+  useEffect(() => {
+    api.get('/products')
+      .then(res => {
+        alert('Connection to backend successful! Products: ' + res.data.length);
+      })
+      .catch(() => {
+        alert('Error connecting to backend!');
+      });
+  }, []);
+
   return (
     <div>
+      Check!
     </div>
   );
 }
