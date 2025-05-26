3632e589173687b6dd646bc3935a8bcd42c70240
Kate <katjas.emailg@gmail.com>
2025-05-26 20:35:57 +0200

    feat: add cookie-parser middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 import cors from 'cors';
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import pool from './db/db';
 import productRoutes from './routes/ProductRoutes';
 import authRoutes from './routes/AuthRoutes';
@@ -10,20 +11,27 @@ import authRoutes from './routes/AuthRoutes';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/products', productRoutes);
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Server is running and connected to the database!');
+});
+
 const startServer = async () => {
   try {
     await pool.connect();
     console.log('Ansluten till databasen!');
-
-    app.use(cors());
-    app.use(express.json());
-    app.use('/products', productRoutes);
-    app.use('/auth', authRoutes);
-
-    app.get('/', (req, res) => {
-      res.send('Server is running and connected to the database!');
-    });
-
     app.listen(PORT, () => {
       console.log(`Ready http://localhost:${PORT}`);
     });
