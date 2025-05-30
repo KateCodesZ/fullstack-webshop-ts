a01068de9b5f17c91ef3afffc47049b73227d2d0
Kate <katjas.emailg@gmail.com>
2025-05-30 08:50:55 +0200

    refactor(App): remove unused test API call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,10 @@
-import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import api from './api';
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Footer from './layout/Footer';
 
 function App() {
-  useEffect(() => {
-    api.get('/products')
-      .then(res => {
-        alert('Connection to backend successful! Products: ' + res.data.length);
-      })
-      .catch(() => {
-        alert('Error connecting to backend!');
-      });
-  }, []);
-
   return (
     <div>
       <Navbar />
